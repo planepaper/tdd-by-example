93ebc5a6960550d5c47059265c06a4e0a2909989
Seungpyo Son <eddysonkr@gmail.com>
2022-07-02 03:57:41 +0900

    feat: Make Equals Function For Value-Object-Pattern
    - Use trianguality at testEquality.
    - Implement Equals Function.

--- src/main/java/Dollar.java
+++ src/main/java/Dollar.java
@@ -11,4 +11,10 @@ public class Dollar
     {
         return new Dollar(amount * multiplier);
     }
+
+    public boolean equals(Object object)
+    {
+        Dollar dollar = (Dollar) object;
+        return amount == dollar.amount;
+    }
 }
