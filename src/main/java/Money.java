3b8148dbe69d301830b4be888dfe94a9a9e469a7
Seungpyo Son <eddysonkr@gmail.com>
2022-07-07 23:24:09 +0900

    feat: Implement detailed abstract Money Class
    - Change Test and Implement.

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,11 +1,23 @@
-public class Money
+abstract public class Money
 {
     protected int amount;
+
+    abstract Money times(int multiplier);
 
     public boolean equals(Object object)
     {
         Money money = (Money) object;
         return amount == money.amount
                 && getClass().equals(money.getClass());
+    }
+
+    static Money dollar(int amount)
+    {
+        return new Dollar(amount);
+    }
+
+    static Money franc(int amount)
+    {
+        return new Franc(amount);
     }
 }
