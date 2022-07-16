4b9830c42f91c95ac27d4b0df791ea2186efd823
Seungpyo Son <eddysonkr@gmail.com>
2022-07-16 22:52:00 +0900

    feat: Make Money Inherit Expression Interface
    - For Add Method.

--- src/main/java/Money.java
+++ src/main/java/Money.java
@@ -1,4 +1,4 @@
-class Money
+class Money implements Expression
 {
     protected int amount;
     protected String currency;
@@ -40,7 +40,7 @@ class Money
         return amount + " " + currency;
     }
 
-    Money plus(Money addend)
+    Expression plus(Money addend)
     {
         return new Money(amount + addend.amount, currency);
     }
