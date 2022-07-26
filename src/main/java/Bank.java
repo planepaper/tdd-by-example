d14703c7d8c8fbece166a5bc7058c64e61306e10
Seungpyo Son <eddysonkr@gmail.com>
2022-07-26 23:17:38 +0900

    feat: Remove overlaping code in reduce method

--- src/main/java/Bank.java
+++ src/main/java/Bank.java
@@ -2,6 +2,8 @@ public class Bank
 {
     Money reduce(Expression source, String to)
     {
-        return Money.dollar(10);
+        Sum sum = (Sum) source;
+        int amount = sum.augend.amount + sum.addend.amount;
+        return new Money(amount, to);
     }
 }
