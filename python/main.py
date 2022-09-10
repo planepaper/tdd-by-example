3a693142824ba5fb0bfdc54b9c7642753ad1d249
planepaper <eddysonkr@gmail.com>
2022-09-10 22:42:34 +0900

    feat: Make Test for Testing TestCase Class

---
+++ python/main.py
@@ -0,0 +1,25 @@
+class TestCase:
+    def __init__(self, name):
+        self.name= name
+
+    def run(self):
+        method = getattr(self, self.name)
+        method()
+
+class WasRun(TestCase):
+    def __init__(self, name):
+        self.wasRun = None
+        TestCase.__init__(self,name)
+    
+    def testMethod(self):
+        self.wasRun = 1
+
+class TestCaseTest(TestCase):
+    def testRunning(self):
+        test = WasRun("testMethod")
+        assert(not test.wasRun)
+        test.run()
+        assert(test.wasRun)
+
+if __name__ == "__main__":
+    TestCaseTest("testRunning").run()
