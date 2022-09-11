6eb379056c26e5eefc6ee8665b7d83e0d095b17a
planepaper <eddysonkr@gmail.com>
2022-09-12 03:16:43 +0900

    feat: Make SetUp Mehthod To Simplify Code

--- python/main.py
+++ python/main.py
@@ -2,24 +2,36 @@ class TestCase:
     def __init__(self, name):
         self.name= name
 
+    def setUp(self):
+        pass
+
     def run(self):
+        self.setUp()
         method = getattr(self, self.name)
         method()
 
 class WasRun(TestCase):
     def __init__(self, name):
-        self.wasRun = None
         TestCase.__init__(self,name)
     
     def testMethod(self):
         self.wasRun = 1
 
+    def setUp(self):
+        self.wasRun = None
+        self.wasSetUp= 1
+
 class TestCaseTest(TestCase):
+    def setUp(self):
+        self.test = WasRun("testMethod")
+        
+    def testSetUp(self):
+        self.test.run()
+        assert(self.test.wasSetUp)
+
     def testRunning(self):
-        test = WasRun("testMethod")
-        assert(not test.wasRun)
-        test.run()
-        assert(test.wasRun)
+        self.test.run()
+        assert(self.test.wasRun)
 
 if __name__ == "__main__":
-    TestCaseTest("testRunning").run()
+    TestCaseTest("testSetUp").run()
