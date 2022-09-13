b17eb1288fd0256c9159d5a2672f53e3e05c45fd
planepaper <eddysonkr@gmail.com>
2022-09-13 22:47:19 +0900

    feat: Change Flags to Logs And Make TearDown

--- python/main.py
+++ python/main.py
@@ -5,29 +5,33 @@ class TestCase:
     def setUp(self):
         pass
 
+    def tearDown(self):
+        pass
+
     def run(self):
         self.setUp()
         method = getattr(self, self.name)
         method()
+        self.tearDown()
 
 class WasRun(TestCase):
     def __init__(self, name):
         TestCase.__init__(self,name)
     
-    def testMethod(self):
-        self.wasRun = 1
-
     def setUp(self):
-        self.wasRun = None
-        self.wasSetUp= 1
+        self.log = "setUp "
+
+    def testMethod(self):
+        self.log = self.log + "testMethod "
+
+    def tearDown(self):
+        self.log = "tearDown "
 
 class TestCaseTest(TestCase):
-    def setUp(self):
-        self.test = WasRun("testMethod")
-        
-    def testSetUp(self):
+    def testTemplateMethod(self):
+        test= WasRun("testmethod")
         self.test.run()
-        assert(self.test.wasSetUp)
+        assert("setUp testMethod tearDown " == self.test.log)
 
     def testRunning(self):
         self.test.run()
