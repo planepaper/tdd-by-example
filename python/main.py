b6fcebc0fc79f9a9d56b1c942a8c3d803a6c0dc5
planepaper <eddysonkr@gmail.com>
2022-09-13 23:15:16 +0900

    feat: Implement TestResult For Success Case

--- python/main.py
+++ python/main.py
@@ -9,10 +9,13 @@ class TestCase:
         pass
 
     def run(self):
+        result = TestResult()
+        result.testStarted()
         self.setUp()
         method = getattr(self, self.name)
         method()
         self.tearDown()
+        return TestResult()
 
 class WasRun(TestCase):
     def __init__(self, name):
@@ -27,15 +30,31 @@ class WasRun(TestCase):
     def tearDown(self):
         self.log = "tearDown "
 
+    def testBrokenMethod(self):
+        raise Exception
+
+class TestResult:
+    def __init__(self):
+        self.runCount = 0
+    def testStarted(self):
+        self.runCount = self.runCount + 1
+    def summary(self):
+        return "%d run, 0 failed" % self.runCount
+
 class TestCaseTest(TestCase):
     def testTemplateMethod(self):
         test= WasRun("testmethod")
-        self.test.run()
-        assert("setUp testMethod tearDown " == self.test.log)
+        result = self.test.run()
+        assert("1 run, 0 failed" == result.summary())
 
     def testRunning(self):
         self.test.run()
         assert(self.test.wasRun)
+
+    def testFailedResult(self):
+        test = WasRun("testBrokenMethod")
+        result = test.run()
+        assert("1 run, 1 failed" == result.summary())
 
 if __name__ == "__main__":
     TestCaseTest("testSetUp").run()
